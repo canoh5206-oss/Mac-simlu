d758c1ba92e76c20bc5d73d188db1d90b9ca8977
canoh5206-oss <canoh5206@gmail.com>
2026-06-28 20:19:25 +0300

    İndex.js

--- index.js
+++ index.js
@@ -390,7 +390,5 @@ client.on('interactionCreate', async (interaction) => {
 
         const [prefix, rolTipi, deleteId] = interaction.customId.split('_');
         if (prefix !== 'k') return;
-});
 
-client.login(process.env.TOKEN);
-        
+        co
